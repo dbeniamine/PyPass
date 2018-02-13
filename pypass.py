fe1c337f9b81040cfa85d0120ab516344a794024
David Beniamine <david@beniamine.net>
2018-02-13 19:45:22 +0100

    Add possibility to hide window after running pass
    If the hide option is on (--hide or click hide button), the window will
    automatically hide after running pass successfully on clipboard mode

--- pypass.py
+++ pypass.py
@@ -29,12 +29,13 @@ from gi.repository import Gtk, Gdk, GObject
 
 class PyPassWindow(Gtk.Window):
 
-    def __init__(self, clipboard, magic, inline_selection, inline_completion):
-        self.copyToClipboard = clipboard
+    def __init__(self, clipboard, magic, inline_selection, inline_completion, should_hide):
+        self.copyToClipboard = clipboard or should_hide
         self.clipboard_next_text = None
         self.magic = magic
         self.magic_output = False
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        self.should_hide = should_hide
 
         Gtk.Window.__init__(self, title="PyPass")
         self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
@@ -79,7 +80,12 @@ class PyPassWindow(Gtk.Window):
 
         self.button_clip = Gtk.CheckButton.new_with_mnemonic("Use _clipboard")
         self.button_clip.connect("toggled", self.on_copy_toggled)
+
+        self.button_hide = Gtk.CheckButton.new_with_mnemonic("_Hide window on success (requires clipboard)")
+        self.button_hide.connect("toggled", self.on_hide_toggled)
+
         self.button_clip.set_active(self.copyToClipboard)
+        self.button_hide.set_active(self.should_hide)
 
         if magic:
             self.button_magic = Gtk.RadioButton.new_with_mnemonic_from_widget(None, "_Magic")
@@ -94,13 +100,14 @@ class PyPassWindow(Gtk.Window):
         self.grid.attach(self.entry, 0, 0, 2, 2)
         self.grid.attach_next_to(self.button_run, self.entry, Gtk.PositionType.RIGHT, 1, 3)
         self.grid.attach_next_to(self.button_clip, self.entry, Gtk.PositionType.BOTTOM, 2, 1)
+        self.grid.attach_next_to(self.button_hide, self.button_clip, Gtk.PositionType.BOTTOM, 1, 1)
 
         if magic:
-            self.grid.attach_next_to(self.button_magic, self.button_clip, Gtk.PositionType.BOTTOM, 1, 1)
+            self.grid.attach_next_to(self.button_magic, self.button_hide, Gtk.PositionType.BOTTOM, 1, 1)
             self.grid.attach_next_to(self.button_more_magic, self.button_magic, Gtk.PositionType.RIGHT, 1, 1)
             prev_button = self.button_magic
         else:
-            prev_button = self.button_clip
+            prev_button = self.button_hide
 
         self.grid.attach_next_to(self.timeout_label, prev_button, Gtk.PositionType.BOTTOM, 1, 1)
         self.grid.attach_next_to(self.button_sleep, self.timeout_label, Gtk.PositionType.RIGHT, 1, 1)
@@ -117,6 +124,15 @@ class PyPassWindow(Gtk.Window):
 
     def on_copy_toggled(self, button):
         self.copyToClipboard = button.get_active()
+        # Deactivate should_hide if clipboard is not used
+        if(self.should_hide):
+            self.button_hide.set_active(self.copyToClipboard)
+
+    def on_hide_toggled(self, button):
+        self.should_hide = button.get_active()
+        # Force use clipboard
+        if(self.should_hide and not self.copyToClipboard):
+            self.button_clip.clicked()
 
     def on_magic_toggled(self, button, name):
         self.magic_output = name == "Magic"
@@ -159,6 +175,9 @@ class PyPassWindow(Gtk.Window):
                     self.clipboard_next_text = self.rand_str(256)
                 self.clipboard.set_text(res, -1)
                 res = "Password copied to clipboard"
+                # Hide window if using clipboard and no error
+                if(self.should_hide):
+                    self.hide()
         else:
             res = output.stderr.decode('utf-8')
 
@@ -174,8 +193,9 @@ class PyPassWindow(Gtk.Window):
 @click.option('--inline_selection/--no-inline_selection', default=True, help="Use inline selection")
 @click.option('--inline_completion/--no-inline_completion', default=True, help="Use inline completion")
 @click.option('--magic/--no-magic', default=False, help="Magic")
-def pypass(magic, clipboard, inline_selection, inline_completion):
-    win = PyPassWindow(clipboard, magic, inline_selection, inline_completion)
+@click.option('--hide/--no-hide', default=False, help="Hide window after running pass successfully (imply --clipboard)")
+def pypass(magic, clipboard, inline_selection, inline_completion, hide):
+    win = PyPassWindow(clipboard, magic, inline_selection, inline_completion, hide)
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
